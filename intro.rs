93cf70eacab0673e058bd2a08fa79dbd48160e37
cruzl.eth <103182232+cruzlx@users.noreply.github.com>
2025-10-30 12:03:50 +0700

    Add
    Removed a line about bugs and giving up too early.

--- intro.rs
+++ intro.rs
@@ -3,7 +3,6 @@
    Every panic teaches resilience, every unsafe block tests your integrity.
    Ownership isn’t just a memory model — it’s a philosophy.
    We build systems that outlive us, and values that self-execute.
-   The only bug we can’t fix is giving up too early.
 */
 
 fn main() {
